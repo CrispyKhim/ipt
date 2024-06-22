9efb7de9ce12652267f274a47edc545ece62cab9
CrispyKhim <christopherkhim668@gmail.com>
2024-06-22 17:59:32 +1000

    added feedback form, catapi render on home.html, created another learn.html for non-users, added info in legal.html,
    also made minor changes such as:
    - renamed variables
    - added icons from Bootstrap
    :grinning:

--- main.py
+++ main.py
@@ -14,18 +14,38 @@ def error():
     return render_template('error.html')
 
 # About Page
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():
-    return render_template('about.html')
+    feedback = False
+    if request.method == 'POST':
+        
+        # Feedback saved
+        feedback = True
+        email = request.form.get('feedback-email') # Email saved
+        comment = request.form.get('feedback-comment') # Comment saved
+        return render_template('about.html', feedback=feedback)
+        
+    else:
+        return render_template('about.html', feedback=feedback)
+
+# Learn Page
+@app.route('/learn')
+def learn():
+    return render_template('learn.html')
+
+# legal Page
+@app.route('/legal')
+def legal():
+    return render_template('legal.html')
 
 
 # Login / Register Page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get('email') # Email saved
-        username = request.form.get('username') # Username saved
-        password = request.form.get('password') # Password saved
+        email = request.form.get('login-email') # Email saved
+        username = request.form.get('login-username') # Username saved
+        password = request.form.get('login-password') # Password saved
         return redirect(url_for('home', username=username))
     else:
         return render_template('login.html')
@@ -33,22 +53,19 @@ def login():
 # Home Page
 @app.route('/home/<string:username>', methods=['GET', 'POST'])
 def home(username):
+    check = False
     if request.method == 'POST':
-        search_term = request.form.get('search')
         check = True
-        return redirect(url_for('search', username=username, search_term=search_term))
+        search_term = request.form.get('search')
+        # Get image from API
+        response = requests.get("https://api.thecatapi.com/v1/images/search?limit=1&random=RAND&api_key='live_ug7L3YzsrAvPthkUAcOVWqHTJfnd4b6u1NFao0EhLllQDwmokv9iEXeUgQOuoljn'").json()
+        # Get the image from the result
+        image_url = response[0]['url']
+        width = response[0]['width'] * 0.9
+        height = response[0]['height'] * 0.9
+        return render_template('/users/Home.html', username=username, search_term=search_term, image_url=image_url, check=check, width=width, height=height)
     else:
-        return render_template('/users/Home.html', username=username)
-
-# Rendering the searched result from user input
-@app.route('/home/<string:username>/search-<string:search_term>', methods=['GET', 'POST'])
-def search(username, search_term):
-    # Get image from API
-    response = requests.get("https://api.thecatapi.com/v1/images/search?limit=1&random=RAND&api_key='live_ug7L3YzsrAvPthkUAcOVWqHTJfnd4b6u1NFao0EhLllQDwmokv9iEXeUgQOuoljn'").json()
-    # Get the image from the result
-    image_url = response[0]['url']
-    return render_template('/users/search.html', username=username, search_term=search_term, image_url=image_url)
-
+        return render_template('/users/Home.html', username=username, check=check)
 
 # Favourites Page
 @app.route('/home/<string:username>/favourites')
@@ -65,9 +82,9 @@ def share(username):
 def connect(username):
     return render_template('/users/connect.html', username=username)
 
-# Learn Page
+# Learn Page - After login
 @app.route('/home/<string:username>/learn')
-def learn(username):
+def Learn_user(username):
     return render_template('/users/learn.html', username=username)
 
 
--- static/javascript/myscripts.js
+++ static/javascript/myscripts.js
@@ -1,24 +1,3 @@
-// Scroll to Top Button
-// Get the button
-let mybutton = document.getElementById("myBtn");
-        
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 // Scroll Down Button (Index)
 $(function() {
   $('a[href*#]').on('click', function(e) {
@@ -26,3 +5,4 @@ $(function() {
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
 });
+
